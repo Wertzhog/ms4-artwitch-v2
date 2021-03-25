83c3471aea0cd9f7c34bff1637658989933a8b43
Wertzhog <ljupko.jurcevic@gmail.com>
2021-03-25 20:30:23 +0000

    added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IYzBvFN6ZQOpXyoFeBqwjeqMSoCwcNVMhVeKVHK8MkfLCN3Ftg65zfdlr9QxIG4TokVI6gAPspS29ijSXWZl6Zo00Cmv2u7tq',
+        'client_secret':'test client secret',
     }
 
     return render(request, template, context)
