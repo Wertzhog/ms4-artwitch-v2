cfd7622fabddc19d39fca6d21fe77ee751f844ad
Wertzhog <ljupko.jurcevic@gmail.com>
2021-03-25 12:47:32 +0000

    Udated checkout models with custom methods

--- checkout/models.py
+++ checkout/models.py
@@ -13,7 +13,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *', null=False, blank=False)
+    country = models.CharField(max_length=50, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -26,6 +26,35 @@ class Order(models.Model):
                                       null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
+    # order number generator
+
+    def generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    # override for original save method
+
+    def save(self, *args, **kwargs):
+
+        if not self.order_number:
+            self.order_number = self.generate_order_number()
+        super().save()
+
+    def __str__(self):
+        return self.order_number
+
+    # update grad total and delivery costs
+
+    def update_total(self):
+
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            sdp = settings.STANDARD_DELIVERY_PERCENTAGE
+            self.delivery_cost = self.order_total * sdp / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
 
 
 class OrderLineItem(models.Model):
@@ -40,3 +69,8 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
                                          editable=False)
+
+    def save(self, *args, **kwargs):
+
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
