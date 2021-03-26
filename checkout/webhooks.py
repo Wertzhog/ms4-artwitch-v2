e86b0585ce530179a09f0e21f9a001b32ea6a973
Wertzhog <ljupko.jurcevic@gmail.com>
2021-03-26 20:13:34 +0000

    complete payment system, removed typos across app

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -11,8 +11,8 @@ import stripe
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
-    wh_secret = settings.STRIPE_WH_SECRET
-    stripe.api_key = settings.STRIPE_SECRET_KEY
+    wh_secret = nonono
+    stripe.api_key = nonono
 
     # Get the webhook data and verify its signature
     payload = request.body
